0d1a7b61105dd30d549562f37dafe83f2aa45d9e
CounterDark <aleksanderkunkowski@gmail.com>
2025-05-24 16:43:26 +0200

    cat animations and simulate exercises

--- catanimation.py
+++ catanimation.py
@@ -12,31 +12,54 @@ pygame.display.set_caption('Animation')
 
 WHITE = (255, 255, 255)
 catImg = pygame.image.load('cat.png')
-catx = 10
-caty = 10
-direction = 'right'
+cat1x = 10
+cat1y = 10
+cat2x = 280
+cat2y = 10
+cat1Direction = 'right'
+cat2Direction = 'left'
 
 while True: # the main game loop
     DISPLAYSURF.fill(WHITE)
 
-    if direction == 'right':
-        catx += 5
-        if catx == 280:
-            direction = 'down'
-    elif direction == 'down':
-        caty += 5
-        if caty == 220:
-            direction = 'left'
-    elif direction == 'left':
-        catx -= 5
-        if catx == 10:
-            direction = 'up'
-    elif direction == 'up':
-        caty -= 5
-        if caty == 10:
-            direction = 'right'
+    # cat 1 direction
+    if cat1Direction == 'right':
+        cat1x += 5
+        if cat1x == 280:
+            cat1Direction = 'down'
+    elif cat1Direction == 'down':
+        cat1y += 5
+        if cat1y == 220:
+            cat1Direction = 'left'
+    elif cat1Direction == 'left':
+        cat1x -= 5
+        if cat1x == 10:
+            cat1Direction = 'up'
+    elif cat1Direction == 'up':
+        cat1y -= 5
+        if cat1y == 10:
+            cat1Direction = 'right'
 
-    DISPLAYSURF.blit(catImg, (catx, caty))
+    #cat 2 directions
+    if cat2Direction == 'right':
+        cat2x += 5
+        if cat2x == 280:
+            cat2Direction = 'up'
+    elif cat2Direction == 'down':
+        cat2y += 5
+        if cat2y == 220:
+            cat2Direction = 'right'
+    elif cat2Direction == 'left':
+        cat2x -= 5
+        if cat2x == 10:
+            cat2Direction = 'down'
+    elif cat2Direction == 'up':
+        cat2y -= 5
+        if cat2y == 10:
+            cat2Direction = 'left'
+
+    DISPLAYSURF.blit(catImg, (cat1x, cat1y))
+    DISPLAYSURF.blit(catImg, (cat2x, cat2y))
 
     for event in pygame.event.get():
         if event.type == QUIT:
